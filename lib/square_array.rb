d61659463c76b6ad07676b0d5718ecd57c320f79
Paige Miles <paige@lucas-miles.com>
2020-09-03 19:24:32 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,4 @@
-#def square_array(array)
- # count = 0
-  #while count < array.length do
-   # array.push(array[count]**array[count])
-    #count += 1
-  #end
-  #p array
-#end
+def square_array(array)
+  array.length.times do |index|
+    array[index].push(array[index]**array[index])
+end
