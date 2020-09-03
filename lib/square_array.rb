af9282b8c2d25a8298fb12dc8e3d6c407557fc88
Paige Miles <paige@lucas-miles.com>
2020-09-03 19:30:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(numbers)
   square_array = []
   numbers.length.times do |index|
-    square_array.push(numbers[index]**)
+    square_array.push(numbers[index] * numbers[index])
 end
 p square_array
 end 
